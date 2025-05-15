b83398396510b4d8c515c140b049a6772ca186dc
kristijanGeorgiev <148566162+kristijanGeorgiev@users.noreply.github.com>
2025-05-15 17:43:27 +0200

    Add files via upload

--- program.cs
+++ program.cs
@@ -1,7 +1,48 @@
+using HotelRoomManagement.Application.Interfaces;
+using HotelRoomManagement.Application.Mappings;
+using HotelRoomManagement.Application.Services;
+using HotelRoomManagement.Infrastructure.Data;
+using HotelRoomManagement.Infrastructure.Interfaces;
+using HotelRoomManagement.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Swagger for API docs/testing
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// AutoMapper
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+// EF Core - Add your SQL Server connection string here
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Register Repositories
 builder.Services.AddScoped<IGuestRepository, GuestRepository>();
 builder.Services.AddScoped<IRoomRepository, RoomRepository>();
+
+// Register Services
 builder.Services.AddScoped<IGuestService, GuestService>();
 builder.Services.AddScoped<IRoomService, RoomService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
